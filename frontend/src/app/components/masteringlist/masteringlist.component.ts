f39fadb07dc265a27903fa2a8fc1c390dcc52247
IB <ib.info@protonmail.com>
2021-12-15 00:52:29 +0100

    fix: display masterings list in MatTable

--- frontend/src/app/components/masteringlist/masteringlist.component.ts
+++ frontend/src/app/components/masteringlist/masteringlist.component.ts
@@ -23,9 +23,9 @@ import { plainToClass } from 'class-transformer';
 })
 
 export class MasteringListComponent implements AfterViewInit, OnDestroy {
-    displayedColumns: string[] = ['name', 'category', 'level', 'actions'];
+    displayedColumns: string[] = ['name', 'category', 'level']; //, 'actions'];
     dataSource: MatTableDataSource<Mastering> = new MatTableDataSource();
-    // filter: string = '';
+    filter: string = '';
     state: MatTableState;
 
     @ViewChild(MatPaginator) paginator!: MatPaginator;
@@ -35,7 +35,7 @@ export class MasteringListComponent implements AfterViewInit, OnDestroy {
     @Input() masterings!: Mastering[];  // masterings: Mastering[] = [];
     @Input() set getUserID(val: number) {
         this.userCvId = val;
-        this.refresh();
+        // this.refresh();
     }
     
     constructor(
@@ -52,13 +52,13 @@ export class MasteringListComponent implements AfterViewInit, OnDestroy {
         this.dataSource.paginator = this.paginator;
         this.dataSource.sort = this.sort;
         // définit le predicat qui doit être utilisé pour filtrer les membres
-    //    // this.dataSource.filterPredicate = (data: Skill, filter: string) => {
-    //    //     const str = data.name + ' ' + data.category?.name;
-    //    //     return str.toLowerCase().includes(filter);
-     //   // };
+        this.dataSource.filterPredicate = (data: Mastering, filter: string) => {
+            const str = data.skill?.name + ' ' + data.skill?.category?.name + ' ' + data.level;
+            return str.toLowerCase().includes(filter);
+        };
         // établit les liens entre le data source et l'état de telle sorte que chaque fois que 
         // le tri ou la pagination est modifié l'état soit automatiquement mis à jour
-        // this.state.bind(this.dataSource);
+        this.state.bind(this.dataSource);
         // récupère les données 
         this.refresh();
     }
@@ -70,22 +70,22 @@ export class MasteringListComponent implements AfterViewInit, OnDestroy {
             // restaure l'état du datasource (tri et pagination) à partir du state
             this.state.restoreState(this.dataSource);
             // restaure l'état du filtre à partir du state
-     //       // this.filter = this.state.filter;
+            this.filter = this.state.filter;
         });
     }
 
-    // // appelée chaque fois que le filtre est modifié par l'utilisateur
-    // filterChanged(e: KeyboardEvent) {
-    //     const filterValue = (e.target as HTMLInputElement).value;
-    //     // applique le filtre au datasource (et provoque l'utilisation du filterPredicate)
-    //     this.dataSource.filter = filterValue.trim().toLowerCase();
-    //     // sauve le nouveau filtre dans le state
-    //     this.state.filter = this.dataSource.filter;
-    //     // comme le filtre est modifié, les données aussi et on réinitialise la pagination
-    //     // en se mettant sur la première page
-    //     if (this.dataSource.paginator)
-    //         this.dataSource.paginator.firstPage();
-    // }
+    // appelée chaque fois que le filtre est modifié par l'utilisateur
+    filterChanged(e: KeyboardEvent) {
+        const filterValue = (e.target as HTMLInputElement).value;
+        // applique le filtre au datasource (et provoque l'utilisation du filterPredicate)
+        this.dataSource.filter = filterValue.trim().toLowerCase();
+        // sauve le nouveau filtre dans le state
+        this.state.filter = this.dataSource.filter;
+        // comme le filtre est modifié, les données aussi et on réinitialise la pagination
+        // en se mettant sur la première page
+        if (this.dataSource.paginator)
+            this.dataSource.paginator.firstPage();
+    }
 
     // // appelée quand on clique sur le bouton "edit" d'un membre
     // edit(user: User) {
