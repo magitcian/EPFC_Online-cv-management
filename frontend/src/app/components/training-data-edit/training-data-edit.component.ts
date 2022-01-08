da895e3a7a7c2a0579a85070ec3aecb5782ce7b2
IB <ib.info@protonmail.com>
2022-01-08 18:48:00 +0100

    feat trainings: add usings (skills) in CV

--- frontend/src/app/components/training-data-edit/training-data-edit.component.ts
+++ frontend/src/app/components/training-data-edit/training-data-edit.component.ts
@@ -2,6 +2,7 @@ import { Component, Input, Output, EventEmitter, ViewChild, ElementRef } from '@
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { TrainingService } from '../../services/training.service';
 import { EnterpriseService } from '../../services/enterprise.service';
+import { SkillService } from '../../services/skill.service';
 import { Training } from 'src/app/models/training';
 import { Enterprise } from 'src/app/models/enterprise';
 import { Skill } from 'src/app/models/skill';
@@ -10,9 +11,12 @@ import { MatDialog } from '@angular/material/dialog';
 import { FormGroup, FormBuilder, Validators, FormControl } from '@angular/forms';
 import * as _ from 'lodash-es';
 import { plainToClass } from 'class-transformer';
-// import {MatChipInputEvent} from '@angular/material/chips';
-// import {COMMA, ENTER} from '@angular/cdk/keycodes';
-// import {MatAutocompleteSelectedEvent} from '@angular/material/autocomplete';
+import {MatChipInputEvent} from '@angular/material/chips';
+import {COMMA, ENTER} from '@angular/cdk/keycodes';
+import {MatAutocompleteSelectedEvent} from '@angular/material/autocomplete';
+import { Observable } from 'rxjs';
+import { map, startWith } from 'rxjs/operators';
+import { Moment } from 'moment';
 
 @Component({
     selector: 'app-training-data-edit', 
@@ -26,20 +30,20 @@ export class TrainingDataEditComponent {
         this.training = val;
         this.controlInput();
     }
-
-    training!: Training;
-
     @Input() isEditable!: boolean;
-
-    experienceId!: number;
-    isEditMode: boolean = false;
-    // training: Training = new Training();
-
     @Input() isNew!: boolean;
 
+    training!: Training;
+    experienceId!: number;
+    isEditMode: boolean = false;
+
     enterprises!: Enterprise[];
-    skills!: Skill[];
-    usings!: Using[];
+    otherSkills: Skill[] = [];
+    trainingUsings: Using[] = [];
+    separatorKeysCodes: number[] = [ENTER, COMMA];
+    filteredSkills: Observable<string[]> = new Observable();
+
+    @ViewChild('skillInput', { static: false }) skillInput: ElementRef<HTMLInputElement> = {} as ElementRef;
 
     @Output() updateTrainingInDaddy: EventEmitter<Training> = new EventEmitter<Training>(); 
     @Output() addTrainingInDaddy: EventEmitter<Training> = new EventEmitter<Training>(); 
@@ -57,33 +61,26 @@ export class TrainingDataEditComponent {
     public ctlEnterpriseId!: FormControl;
     public ctlEnterpriseName!: FormControl;
     public ctlUsings!: FormControl;
+    public skillCtrl = new FormControl();
 
     constructor(
         private fb: FormBuilder,
         private trainingService: TrainingService,
         private enterpriseService: EnterpriseService,
+        private skillService: SkillService,
         public dialog: MatDialog, 
         public snackBar: MatSnackBar) {
        
     }
 
-    // refresh() {
-    //     this.trainingService.getById(this.experienceId).subscribe(training => {
-    //         if (training != null) {
-    //             this.training = training;
-    //         }
-    //         this.controlInput();
-    //     });
-    // }
-
     controlInput() {
-        this.ctlStart = this.fb.control('', []);
-        this.ctlFinish = this.fb.control('', []);
-        this.ctlTitle = this.fb.control('', []);
+        this.ctlStart = this.fb.control('', [Validators.required]);
+        this.ctlFinish = this.fb.control('', [Validators.required, this.validateFinishDate()]);
+        this.ctlTitle = this.fb.control('', [Validators.required]);
         this.ctlDescription = this.fb.control('', []); 
         this.ctlGrade = this.fb.control('', []);
         this.ctlEnterprise = this.fb.control('', []);
-        this.ctlEnterpriseId = this.fb.control('', []);
+        this.ctlEnterpriseId = this.fb.control('', [Validators.required]);
         this.ctlEnterpriseName = this.fb.control('', []);
         this.ctlUsings = this.fb.control('', []);
         // fb.group content needs to respect the JSON we get from backend !
@@ -99,15 +96,10 @@ export class TrainingDataEditComponent {
                 id: this.ctlEnterpriseId,
                 name: this.ctlEnterpriseName
             })
-            // ,usings: this.fb.group({
-            //     id: this.ctlUsingId,
-            //     skill: this.ctlSkill,
-            //     experienceId: this.ctlExperienceId,
-            //     skillId: this.ctlSkillId
-            // })
             ,usings: this.ctlUsings
         })
         this.addEnterprisesInDropDown();
+        this.refreshSkills();
         this.frm.patchValue(this.training);
     }
 
@@ -118,26 +110,119 @@ export class TrainingDataEditComponent {
         });
     }
 
-    update() { // does not update "usings"
-        var res = this.frm.value; // récupère le formulaire avec les infos modifiées
-        this.updateTrainingInDaddy.emit(res); // res est un EventEmitter et celui-ci va être transformé en Training
+    refreshSkills() { // we take all skills and remove the ones in usings so that we only keep the ones that the user does not have
+        this.skillService.getAll().subscribe(skills => {
+            this.otherSkills = skills;
+            this.trainingUsings = [];
+            this.training.usings?.forEach(u =>
+                this.trainingUsings.push(u)
+            );
+            for (let s = 0; s < this.otherSkills.length; s++) {
+                for (let u = 0; u < this.trainingUsings.length; u++) {
+                    if (this.otherSkills[s]?.id == this.trainingUsings[u].skill?.id) {
+                        this.otherSkills.splice(s, 1) //supprime 1 élément à l'index s
+                    }
+                }
+            }
+            this.updateFilter();
+        });
     }
 
-    add() { 
-        var res = this.frm.value; // récupère le formulaire avec les infos modifiées
-        res.id = 0;
-        _.assign(this.training, res);    // dit que le res est formaté comme un training (il faut le repréciser)
-        res = plainToClass(Training, res); 
-        this.addTrainingInDaddy.emit(res); // res est un EventEmitter et celui-ci va être transformé en Training
+    updateFilter() {
+        this.filteredSkills = this.skillCtrl.valueChanges.pipe(
+            startWith(null),
+            map((skill: string | null) => (skill ? this._filter(skill) : this.otherSkills.map(s => s.name!))),
+        );
     }
 
-    changeEditMode() {
+    private _filter(value: string): string[] {
+        const filterValue = value.toLowerCase();
+        return this.otherSkills.map(s => s.name!).filter(skill => skill.toLowerCase().includes(filterValue));
+    }
+
+    addSkill(event: MatChipInputEvent): void {
+
+    }
+
+    removeSkill(using: Using): void {
+        const index = this.trainingUsings.indexOf(using);
+        if (index >= 0) {
+            let u = this.trainingUsings.splice(index, 1);
+            this.otherSkills.splice(0, 0, u[0].skill!);
+            this.updateFilter();
+            this.frm.markAsDirty();
+        }
+    }
+
+    selectedSkill(event: MatAutocompleteSelectedEvent): void {
+        var skill = this.otherSkills.find(s => s.name == event.option.viewValue);
+        if (skill) {
+            const index = this.otherSkills.indexOf(skill);
+            if (index >= 0) {
+                let s = this.otherSkills.splice(index, 1);
+                let u = new Using();
+                u.addSkill(s[0], this.training);
+                this.trainingUsings.splice(0, 0, u);
+                this.frm.markAsDirty();
+            }
+            this.skillInput.nativeElement.value = '';
+            this.skillCtrl.setValue(null);
+        }
+    }
+
+    edit() {
+        this.addEnterprisesInDropDown();
+        var res = this.frm.value;
+        _.assign(this.training, res);
+        res = plainToClass(Training, res);
+        res.usings = this.trainingUsings;
+        //console.log(res);
+        if (this.isNew) {
+            res.id = 0;
+            this.addTrainingInDaddy.emit(res);
+        } else {
+            this.updateTrainingInDaddy.emit(res);
+        }
+    }
+
+    cancel() {
         if (this.isNew) {
             this.changeAddModeInDaddy.emit();
         } else {
             this.changeUpdateModeInDaddy.emit();
         }
     }
+
+    validateFinishDate(): any {
+        return (ctl: FormControl) => {
+            const finishDate: Moment = ctl.value;
+            const startDate: Moment = this.ctlStart.value;
+            if (finishDate < startDate)
+                return { finishDateEarlierThanStartDate: true }
+            return null;
+        };
+    }
+
+    // update() { // does not update "usings"
+    //     var res = this.frm.value; // récupère le formulaire avec les infos modifiées
+    //     this.updateTrainingInDaddy.emit(res); // res est un EventEmitter et celui-ci va être transformé en Training
+    // }
+
+    // add() { 
+    //     var res = this.frm.value; // récupère le formulaire avec les infos modifiées
+    //     res.id = 0;
+    //     _.assign(this.training, res);    // dit que le res est formaté comme un training (il faut le repréciser)
+    //     res = plainToClass(Training, res); 
+    //     this.addTrainingInDaddy.emit(res); // res est un EventEmitter et celui-ci va être transformé en Training
+    // }
+
+    // changeEditMode() {
+    //     if (this.isNew) {
+    //         this.changeAddModeInDaddy.emit();
+    //     } else {
+    //         this.changeUpdateModeInDaddy.emit();
+    //     }
+    // }
 
     // edit() {
     //     if (this.isNew) {
